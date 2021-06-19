ee8650d947038deeacc869a0908d5581661bce29
Shaheb Ali <systembugbd@gmail.com>
2021-06-19 12:33:48 +0600

    Formatting

--- queue/queueUsingLinkList.js
+++ queue/queueUsingLinkList.js
@@ -1,66 +1,66 @@
-class Node{
+class Node {
      constructor ( data, next = null ) {
           this.data = data;
           this.next = next;
      }
 
-     
+
 }
 
-class queueUsingLinkList{
+class queueUsingLinkList {
      constructor () {
           this.head = null;
           this.tail = null
           this.size = 0;
-        
-               
+
+
      }
 
      enQueueStack( data ) {
-          const node = new Node(data);
+          const node = new Node( data );
           this.size++;
-  
-          if (this.head === null) {
-              this.tail = node;
+
+          if ( this.head === null ) {
+               this.tail = node;
           }
-  
+
           node.next = this.head;
-  
+
           this.head = node;
-        
+
      }
 
-     dQueueStack( ) {
+     dQueueStack() {
           const tailNode = this.tail;
           let current = this.head;
-  
-          if (current === null) {
-              return null;
+
+          if ( current === null ) {
+               return null;
           }
-  
+
           this.szie--;
-  
-          if (current.next === null) {
-              this.clearStack();
-              
-              return tailNode;
+
+          if ( current.next === null ) {
+               this.clearStack();
+
+               return tailNode;
           }
-  
-          while (current.next.next !== null) {
-              current = current.next;
+
+          while ( current.next.next !== null ) {
+               current = current.next;
           }
-  
+
           current.next = null;
           this.tail = current;
-  
+
           return tailNode;
-          
+
      }
 
      clearStack() {
           this.head = null;
           this.size = 0;
-          console.log("Stack is Cleard")
+          console.log( "Stack is Cleard" )
           return null;
      }
 
@@ -69,7 +69,7 @@ class queueUsingLinkList{
           let count = 0;
 
           while ( current ) {
-               console.log( current.data)
+               console.log( current.data )
                current = current.next;
                count++;
           }
@@ -77,12 +77,12 @@ class queueUsingLinkList{
 }
 
 const ql = new queueUsingLinkList()
- 
-ql.enQueueStack('Shaheb')
-ql.enQueueStack('Ali')
-ql.enQueueStack('duplicate')
-ql.enQueueStack("programmer")
- 
+
+ql.enQueueStack( 'Shaheb' )
+ql.enQueueStack( 'Ali' )
+ql.enQueueStack( 'duplicate' )
+ql.enQueueStack( "programmer" )
+
 ql.dQueueStack()
 ql.dQueueStack()
 ql.dQueueStack()
@@ -92,5 +92,5 @@ ql.printStackNode()
 
 
 
-// sl.printStackNode()
-// console.log( sl )
+// ql.printStackNode()
+// console.log( ql )
