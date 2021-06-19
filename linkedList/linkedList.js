46108569fd502ee80a55360277a6dd4a3a0310fb
Shaheb Ali <systembugbd@gmail.com>
2021-06-19 10:43:53 +0600

    LinkedList Complete

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -61,8 +61,54 @@ class LinkedList {
           }
           node.next = current;
           previous.next = node;
-          console.log( previous )
+          // console.log( previous )
           this.size++
+     }
+     
+     getAt( index ) {
+          let current = this.head
+          let count = 0;
+
+          while ( current ) {
+               if ( count == index ) {
+                    console.log(current.data)
+               }
+               count++;
+               current = current.next;
+          }
+          return null;
+     }
+
+     removeAt( index ) {
+          if ( index > 0 && index > this.size ) {
+               return;
+          }
+          let current = this.head;
+          let previous;
+          let count = 0;
+          
+          if(index === 0 )
+          {
+               this.head = current.next
+               console.log('I am here...')
+          } else {
+               console.log('Data', current.next.data, 'is removed from index', index)
+               while ( count < index ) {
+                    count++;
+                    previous = current
+                    current = current.next
+               }
+
+               previous.next = current.next;
+              
+          }
+         this.size--;
+     }
+
+     setEmptyLinkedList() {
+          this.head = null
+          this.size = 0;
+          console.log('LinkedList are cleared, this size is ', this.size, 'and this head is', this.head)
      }
 
      printList() {
@@ -75,15 +121,27 @@ class LinkedList {
 }
 
 let ll = new LinkedList();
+//add in first
 ll.addNodeInFirst( 300 );
 ll.addNodeInFirst( 200 );
 ll.addNodeInFirst( 100 );
 ll.addNodeInFirst( 50 );
-
+//add in last
 ll.addNodeInLast( 500 );
 
+//add in at position
 ll.addAt( 600, 0 )
 ll.addAt( 700, 3 )
 
+//get at position
+ll.getAt( 3 )
+
+//remove from at postion
+ll.removeAt(1)
+
+//print all list data
 ll.printList();
+
+//clear all linked list
+ll.setEmptyLinkedList()
 
